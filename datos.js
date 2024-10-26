e56a69ac336ae46abc4223d8f74d50616cce0ef6
Paz Rodriguez <pazrodriguez1976@gmail.com>
2024-10-26 12:30:18 -0300

    Agregue datos 2

--- datos.js
+++ datos.js
@@ -5,3 +5,11 @@ export const inventario = [
     { id: 2, nombre: "PantalónC", tipo: "pantalón", talla: "L", precio: 30000, cantidad: 5 },
     { id: 3, nombre: "CamisaL", tipo: "camisa", talla: "XL", precio: 40000, cantidad: 2 }
   ];
+
+  export const clientes = [
+    { id: 1, nombre: "Carlos García", correo: "carlos@gmail.com" },
+    { id: 2, nombre: "Ana Pérez", correo: "ana@gmail.com" }
+  ];
+  
+  export const pedidos = []; // Se agregarán los pedidos aquí
+  
