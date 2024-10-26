c74852bb6d78dc81daeed74bef27fa526e8893e6
Mariela Presa <matematica.4.2021@gmail.com>
2024-10-26 12:29:15 -0300

    agregue datos 1

---
+++ datos.js
@@ -0,0 +1,7 @@
+// datos.js
+
+export const inventario = [
+    { id: 1, nombre: "RemeraV", tipo: "remera", talla: "M", precio: 20000, cantidad: 10 },
+    { id: 2, nombre: "PantalónC", tipo: "pantalón", talla: "L", precio: 30000, cantidad: 5 },
+    { id: 3, nombre: "CamisaL", tipo: "camisa", talla: "XL", precio: 40000, cantidad: 2 }
+  ];
